5dd79869b32c4d544f4243eecfa0d28fa830996e
Jonathan Zrake <jonathan.zrake@gmail.com>
2019-01-03 12:03:08 -0500

    Add directory for inclusion as a juce module

---
+++ juce_ndarray/juce_ndarray.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               juce_ndarray
+  vendor:           Jonathan Zrake
+  version:          0.0.1
+  name:             ndarray
+  description:      Numpy-like multidimensional array template in pure C++14
+  website:          https://github.com/jzrake/ndarray
+  license:          GPL
+  dependencies:
+
+END_JUCE_MODULE_DECLARATION
+******************************************************************************/
+
+
+#pragma once
+#define JUCE_NDARRAY_INCLUDED
+#include <juce_ndarray/ndarray.hpp>
